8b09ed8e3dea6bd61c6a157b77be3ed1430d8c62
KIM <tjdghks7373@naver.com>
2025-02-26 10:48:27 +0900

    Initial commit from Create Next App

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
           alt="Next.js logo"
           width={180}
           height={38}
-          priority
+          priority 
         />
         <ol>
           <li>
