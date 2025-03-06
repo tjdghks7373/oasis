33eb7aaef1c3701e1676ae0b11a011f271256510
KIM <tjdghks7373@naver.com>
2025-03-06 17:59:26 +0900

    메인 컨텐츠 진행 중

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,11 @@
 "use client";
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
 import { useEffect, useState } from "react";
+import { Box, Typography } from '@mui/material';
+import { styled } from 'styled-components';
+import Image from 'next/image';
 
 export default function Home() {
   const [movies, setMovies] = useState([]);
@@ -7,7 +13,7 @@ export default function Home() {
     const fetchMovies = async () => {
       try {
         const res = await fetch(
-          `https://api.themoviedb.org/3/movie/now_playing?api_key=${process.env.NEXT_PUBLIC_TMDB_API_KEY}&language=ko-KR&page=2`
+          `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.NEXT_PUBLIC_TMDB_API_KEY}&language=ko-KR&page=1`
         );
         const data = await res.json();
         setMovies(data.results);
@@ -19,15 +25,47 @@ export default function Home() {
     fetchMovies();
   }, []);
   return (
-    <div>메인
-      <ul>
-        {movies.map((movie) => (
-          <li key={movie.id}>
-            {movie.title}
-            
-          </li>
+    <StyledMainContents>
+      <Typography sx={{fontSize:20,fontWeight:700,paddingBottom:'12px'}} variant="h2">인기 영화</Typography>
+      <Swiper spaceBetween={16} slidesPerView={5} pagination={{ clickable: false }} navigation={true}>
+        {movies.map((movie, index) => (
+          <SwiperSlide key={movie.id}>
+            <StyledPosterImg>
+              <StyledNum>{index+1}</StyledNum>
+              <Image
+                src={"https://image.tmdb.org/t/p/w500" + movie.poster_path}
+                width={250}
+                height={356}
+                alt={movie.title}
+              />
+            </StyledPosterImg>
+          </SwiperSlide>
         ))}
-      </ul>
-    </div>
+      </Swiper>
+    </StyledMainContents>
   );
 }
+
+const StyledMainContents = styled(Box)`
+  width:1320px;
+  margin:30px auto
+`;
+
+const StyledPosterImg = styled(Box)`
+  position:relative;  
+  overflow:hidden;
+  width:100%;
+  border-radius:5px;
+`;
+
+const StyledNum = styled(Box)`
+  position:absolute;
+  top:6px;
+  left:6px;
+  width:28px;
+  height:28px;
+  background-color:#000;
+  color:#fff;
+  text-align:center;
+  
+`;
